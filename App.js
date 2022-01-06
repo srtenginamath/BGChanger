e0e921f24df722fb747f634f686a367c8de21640
srtenginamath <srtenginamath1999@gmail.com>
2022-01-06 13:31:27 +0530

    Reset color button add

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  StatusBar,
+} from "react-native";
 
-export default function App() {
+const App = () => {
+  const [randomColor, setRandomColor] = useState("rgb(32, 0, 126)");
+
+  const changeBG = () => {
+    let color = "rgb(" +
+      Math.floor(Math.random() * 256) +
+      "," +
+      Math.floor(Math.random() * 256) +
+      "," +
+      Math.floor(Math.random() * 256) +
+      ")";
+
+    setRandomColor(color);
+  };
+   
+ 
+
+  const changeReset = () => {
+    let color = "#000";
+
+    setRandomColor(color);
+  };
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar backgroundColor={randomColor} />
+      <View style={[styles.container, { backgroundColor: randomColor }]}>
+        <TouchableOpacity onPress={changeBG}>
+          <Text style={styles.text}>Tap Me</Text>
+          
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={changeReset}>
+          <Text style={styles.text1}>Reset</Text>
+          
+        </TouchableOpacity>
+        
+      </View>
+    </>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
+  text: {
+    fontSize: 30,
+    backgroundColor: "#BB2CD9",
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    color: "#FFFFFF",
+    borderRadius: 15,
+    textTransform: "uppercase",
+  },
+
+  text1: {
+    fontSize: 30,
+    backgroundColor: "#23FF",
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    color: "#FFFFFF",
+    marginTop:5,
+    borderRadius: 15,
+    textTransform: "uppercase",
+  },
+
+  
 });
